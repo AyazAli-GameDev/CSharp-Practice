96b56272cebf40624e19070d80add320a4e101d5
Ayaz Ali <ayazali04chandio@gmail.com>
2026-04-03 20:13:34 +0500

    Create PrintHello.cs
    Added code that print Hello with name

---
+++ PrintHello.cs
@@ -0,0 +1,8 @@
+class PrintHello
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your name : ");
+        Console.WriteLine("Hello " + Console.ReadLine());
+    }
+}
